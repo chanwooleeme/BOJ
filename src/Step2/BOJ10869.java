7422d0e7cc9279422241d79c27f35e63607d78d7
ToothColdWoo <upup1810@naver.com>
2021-04-12 17:12:33 +0900

    10869

---
+++ src/Step2/BOJ10869.java
@@ -0,0 +1,15 @@
+package Step2;
+import java.util.Scanner;
+
+public class BOJ10869 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
