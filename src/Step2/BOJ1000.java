05ae0783823c4d17f14d6df808d022c8ab0713a1
ToothColdWoo <upup1810@naver.com>
2021-04-12 16:45:25 +0900

    Solve BOJ1000

--- src/Step2/BOJ1000.java
+++ src/Step2/BOJ1000.java
@@ -1,2 +1,14 @@
-package Step2;public class BOJ1000 {
+package Step2;
+
+import java.util.Scanner;
+
+public class BOJ1000 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = 0;
+        int b = 0;
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        System.out.println(a + b);
+    }
 }
